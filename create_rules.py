7e4e7b212b15c3ad72051ab0c9391b2e3862cc04
liebesu <lichunjiang42@gmail.com>
2016-09-05 17:42:14 +0800

    update

--- create_rules.py
+++ create_rules.py
@@ -6,16 +6,20 @@ from third.YaraGenerator.yaraGenerator import yaramain
 def get_config():
     config=ConfigParser.ConfigParser()
     config.read(os.path.join(YARA_ROOT,"conf","conf.conf"))
-    input_path=config.get(section, option)
+    input_path=config.get("input", "file_path")
+    output_path=config.get("output", "rule_path")
+    author=config.get("auther", "auther")
     
-    output_path=config.get(section, option)
-    author=config.get(section, option)
+def get_path(input_path):
+    roots,dirs,files =os.walk(input_path)
+    for root in roots:
+        print root 
     
-def get_path():
-    pass
+        
 def get_file_type():
     pass
 
 if __name__=="__main__":
+    
     yaramain(InputDirectory, RulesName, Author, Description, Tags, Verbose, FileType)
     
